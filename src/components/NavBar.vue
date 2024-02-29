0a6435c590d02683b0ac92986078444ad6eeeefb
jolamemushaj <jolamemushaj@gmail.com>
2024-02-29 11:59:45 +0100

    Added mobile version

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,7 +14,60 @@ import {RouterLink} from 'vue-router'
         <!--    </div>-->
 
 
-        <div class="relative">
+
+        <!----------------- MOBILE---------------->
+        <nav class="bg-dark-ecstasy block xl:hidden">
+            <div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8 bg-ecstasy">
+                <div class="flex h-16 items-center justify-between">
+                    <img alt="logo" class="w-40 h-24" src="@/images/second-nav-logo.svg" width="125" height="125"/>
+
+                    <div class="-mr-2 flex xl:hidden">
+                        <!-- Mobile menu button -->
+                        <button type="button"
+                                class="relative inline-flex items-center justify-center rounded-md p-1 text-cinnamonMilkDark hover:bg-cinnamonMilkDark hover:text-dark-tile focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cinnamonMilkDark"
+                                aria-controls="mobile-menu"
+                                aria-expanded="false"
+                        >
+                            <span class="absolute -inset-0.5"></span>
+                            <span class="sr-only">Open main menu</span>
+                            <!--
+                              Icon when menu is closed.
+
+                              Menu open: "hidden", Menu closed: "block"
+                            -->
+                            <svg class="block h-12 w-12" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                            </svg>
+                            <!--
+                              Icon when menu is open.
+
+                              Menu open: "block", Menu closed: "hidden"
+                            -->
+                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Mobile menu, show/hide based on menu state. -->
+            <div class="xl:hidden" id="mobile-menu">
+                <div class="space-y-1 px-2 pb-3 pt-6 bg-tile">
+                    <a href="#" class="block rounded-md bg-cinnamonMilkDark px-3 py-2 text-lg font-medium text-dark-tile">Home</a>
+                    <a href="#" class="block rounded-md px-3 py-2 text-lg font-medium text-cinnamonMilkDark hover:bg-cinnamonMilkDark hover:text-dark-tile">Books</a>
+                    <a href="#" class="block rounded-md px-3 py-2 text-lg font-medium text-cinnamonMilkDark hover:bg-cinnamonMilkDark hover:text-dark-tile">Events</a>
+                    <a href="#" class="block rounded-md px-3 py-2 text-lg font-medium text-cinnamonMilkDark hover:bg-cinnamonMilkDark hover:text-dark-tile">About Us</a>
+                    <a href="#" class="block rounded-md px-3 py-2 text-lg font-medium text-cinnamonMilkDark hover:bg-cinnamonMilkDark hover:text-dark-tile">Contact</a>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+        <!----------------- DESKTOP---------------->
+        <div class="relative hidden xl:block">
             <div class="flex justify-center bg-tile text-xl text-cinnamonMilkDark py-3.5">
                 Sign up and enjoy 10% discount on all your purchases
             </div>
@@ -39,21 +92,25 @@ import {RouterLink} from 'vue-router'
                     </div>
                 </div>
             </div>
-            <div class="bg-cinnamonMilkDark pb-32">
+            <div class="bg-cinnamonMilkDark md:pb-32">
                 <div class="flex flex-row justify-between px-32">
                     <img alt="logo" class="w-80 h-48" src="@/images/first-nav-logo.svg" width="125" height="125"/>
                     <img alt="logo" class="w-2/3 h-48" src="@/images/second-nav-logo.svg" width="125" height="125"/>
                 </div>
                 <div class="flex flex-row">
                     <img alt="logo" class="absolute raft-libra-01  w-96" src="@/images/raft-me-libra.svg"/>
-                    <img alt="logo" class="absolute raft-llambe top-80 right-80  w-96 h-80" src="../images/raft-me-llambe.svg"/>
-                    <img alt="logo" class="absolute vazo-lule w-80 h-48" src="@/images/vazo-luleje.svg"/>
+                    <img alt="logo" class="absolute raft-llambe top-80 right-80  w-96 h-80"
+                         src="../images/raft-me-llambe.svg"/>
                     <img alt="logo" class="absolute raft-lule" src="../images/raft-me-lule.svg"/>
-                    <img alt="logo" class="absolute raft-libra-02 w-96 h-80" src="@/images/raft-me-libra.svg"/>
-                    <img alt="logo" class="absolute raft-libra-03 top-80 -right-0 w-96 h-80" src="@/images/raft-me-libra.svg"/>
+                    <img alt="logo" class="absolute vazo-lule w-40" src="@/images/vazo-luleje.svg"/>
+                    <img alt="logo" class="hidden md:block absolute raft-libra-02 w-96"
+                         src="@/images/raft-me-libra.svg"/>
+                    <img alt="logo" class="hidden md:block  absolute raft-libra-03 top-80 -right-0 w-96"
+                         src="@/images/raft-me-libra.svg"/>
+                    <img alt="logo" class="absolute mace-gri h-28 z-50" src="@/images/mace-gri.svg"/>
                 </div>
             </div>
-            <div class="relative z-50 flex flex-row items-center justify-center space-x-32 py-2.5 bg-dark-ecstasy">
+            <div class="relative grid grid-cols-5 gap-20 items-center justify-center py-2.5 px-40 bg-dark-ecstasy">
                 <div class="uppercase font-bold text-2xl font-serif text-cinnamonMilkDark">Home</div>
                 <div class="uppercase font-bold text-2xl font-serif text-cinnamonMilkDark">Books</div>
                 <div class="uppercase font-bold text-2xl font-serif text-cinnamonMilkDark">Events</div>
@@ -69,26 +126,36 @@ import {RouterLink} from 'vue-router'
     top: 22rem;
     left: -3rem;
 }
+
 .raft-libra-02 {
-    top: 15rem;
-    left: 56rem;
+    top: 22rem;
+    left: 55rem;
 }
+
 .raft-llambe {
     top: 16rem;
     left: 10rem;
 }
+
 .vazo-lule {
-    top: 16.4rem;
-    left: 23rem;
+    top: 15.5rem;
+    left: 28rem;
 }
+
 .raft-lule {
-    top: 2rem;
-    left: 58.2rem;
-    height: 60rem;
-    weight: 60rem;
+    top: 19.8rem;
+    right: 0rem;
+    height: 8rem;
 }
+
 .raft-libra-03 {
-    top: 15.6rem;
+    top: 22rem;
     left: 35rem;
+}
+
+.mace-gri {
+    z-index: 50;
+    top: 24.27rem;
+    right: 4.2rem;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,7 @@
 
 <template>
     <main>
-        <div class="mb-40">
-
+        <div class="">
             <div class="relative mt-8 w-3/5 m-auto">
                 <img alt="logo" class="absolute z-50 h-32 left-12 -top-1.5" src="@/images/mace.svg"/>
                 <input type="text" name="name" id="name"
@@ -13,33 +12,33 @@
             </div>
 
 
-            <div class="grid grid-cols-2 gap-12 pt-44 mx-12 ">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-12 pt-44 mx-12 ">
                 <div class="flex flex-row justify-between bg-cinnamonMilkDark">
                     <div class="flex flex-col space-y-4 self-center items-center w-1/2 px-4">
-                        <div class="text-5xl text-tile">
+                        <div class="text-xl md:text-5xl text-tile">
                             Albanian titles
                         </div>
                         <button type="button"
-                                class="w-1/3 rounded-lg bg-cinnamonMilkDark px-1 py-1 text-lg text-tile shadow-sm ring-2 ring-inset ring-tile hover:bg-cinnamonMilk">
+                                class="md:w-1/3 rounded-lg bg-cinnamonMilkDark px-1 py-1 md:text-lg text-tile shadow-sm ring-2 ring-inset ring-tile hover:bg-cinnamonMilk">
                             Click here
                         </button>
 
                     </div>
-                    <img alt="logo" class=" w-1/2" src="@/images/photo01.jpeg" width="125" height="125"/>
+                    <img alt="logo" class=" w-1/2" src="@/images/photo01.jpeg"/>
                 </div>
 
                 <div class="flex flex-row justify-between bg-cinnamonMilkDark">
                     <div class="flex flex-col space-y-4 self-center items-center w-1/2 px-4">
-                        <div class="text-5xl text-tile">
+                        <div class="text-xl md:text-5xl text-tile">
                             Foreign titles
                         </div>
                         <button type="button"
-                                class="w-1/3 rounded-lg bg-cinnamonMilkDark px-1 py-1 text-lg text-tile shadow-sm ring-2 ring-inset ring-tile hover:bg-cinnamonMilk">
+                                class="md:w-1/3 rounded-lg bg-cinnamonMilkDark px-1 py-1 md:text-lg text-tile shadow-sm ring-2 ring-inset ring-tile hover:bg-cinnamonMilk">
                             Click here
                         </button>
 
                     </div>
-                    <img alt="logo" class=" w-1/2" src="@/images/photo02.jpeg" width="125" height="125"/>
+                    <img alt="logo" class=" w-1/2" src="@/images/photo02.jpeg"/>
                 </div>
             </div>
 
@@ -71,33 +70,183 @@
                     </div>
                 </div>
 
-                <div class="grid grid-cols-5 gap-12 pb-8">
+                <div class="grid grid-cols-1 md:grid-cols-5 gap-12 pb-8">
                     <div class="">
-                        <img alt="logo" class="w-80 h-96" src="@/images/photo03.jpeg" width="125" height="125"/>
-                        <div class="font-semibold text-lg text-center">To kill a mockingbird</div>
+                        <img alt="logo" class="w-80 h-96" src="@/images/photo03.jpeg"/>
+                        <div class="pt-4 tracking-wider font-semibold text-base text-center uppercase">To kill a
+                            mockingbird
+                        </div>
                         <div class="font-light tracking-wider text-center">Harper Lee</div>
                     </div>
                     <div class="">
-                        <img alt="logo" class="w-80 h-96" src="@/images/photo04.jpeg" width="125" height="125"/>
-                        <div class="font-semibold text-lg text-center">Pride and Prejudice</div>
+                        <img alt="logo" class="w-80 h-96" src="@/images/photo04.jpeg"/>
+                        <div class="pt-4 tracking-wider font-semibold text-base text-center uppercase">Pride and
+                            Prejudice
+                        </div>
                         <div class="font-light tracking-wider text-center">Jane Austen</div>
                     </div>
                     <div class="">
-                        <img alt="logo" class="w-80 h-96" src="@/images/photo05.jpeg" width="125" height="125"/>
-                        <div class="font-semibold text-lg text-center">Crime and Punishment</div>
+                        <img alt="logo" class="w-80 h-96" src="@/images/photo05.jpeg"/>
+                        <div class="pt-4 tracking-wider font-semibold text-base text-center uppercase">Crime and
+                            Punishment
+                        </div>
                         <div class="font-light tracking-wider text-center">Fyodor Dostoyevsky</div>
                     </div>
                     <div class="">
-                        <img alt="logo" class="w-80 h-96" src="@/images/photo06.jpeg" width="125" height="125"/>
-                        <div class="font-semibold text-lg text-center">1984</div>
+                        <img alt="logo" class="w-80 h-96" src="@/images/photo06.jpeg"/>
+                        <div class="pt-4 tracking-wider font-semibold text-base text-center uppercase">1984</div>
                         <div class="font-light tracking-wider text-center">George Orwell</div>
                     </div>
                     <div class="">
-                        <img alt="logo" class="w-80 h-96" src="@/images/photo07.jpeg" width="125" height="125"/>
-                        <div class="font-semibold text-lg text-center">Great Expectations</div>
+                        <img alt="logo" class="w-80 h-96" src="@/images/photo07.jpeg"/>
+                        <div class="pt-4 tracking-wider font-semibold text-base text-center uppercase">Great
+                            Expectations
+                        </div>
                         <div class="font-light tracking-wider text-center">Charles Dickens</div>
                     </div>
                 </div>
+            </div>
+
+            <div class="mt-12 bg-cinnamonMilk px-12">
+                <div class="py-12 text-center text-5xl font-semibold font-serif">New Arrivals</div>
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-12 pb-8">
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo08.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">BOX SET</div>
+                        <div class="font-light tracking-wider text-center">Diary of a Wimpy Kid Box of Books</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo09.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">BOOK BUNDLE</div>
+                        <div class="font-light tracking-wider text-center">Vintage Quarterbound Classics Collection I
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo10.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">BOOK BUNDLE</div>
+                        <div class="font-light tracking-wider text-center">Murakami Collectible Classics II</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo11.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">BOOK BUNDLE</div>
+                        <div class="font-light tracking-wider text-center">Page Turners Fantasy Romance Bundle</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-12 bg-cinnamonMilk px-12">
+                <div class="py-12 text-center text-5xl font-semibold font-serif">Bestsellers</div>
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-12 pb-8">
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo12.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">The Heaven & Earth Grocery Store</div>
+                        <div class="font-light tracking-wider text-center">James McBride, Dominic Hoffman, et al.</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo13.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">The Covenant of Water</div>
+                        <div class="font-light tracking-wider text-center">Abraham Verghese</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo14.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">Lessons in Chemistry</div>
+                        <div class="font-light tracking-wider text-center">Bonnie Garmus</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo15.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">The Lost Bookshop</div>
+                        <div class="font-light tracking-wider text-center">Evie Woods</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-12 bg-cinnamonMilk px-12">
+                <div class="py-12 text-center text-5xl font-semibold font-serif">Events</div>
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-12 pb-8">
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo16.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">Blind Date</div>
+                        <div class="font-light tracking-wider text-center">Valentine’s day</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo17.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">Bring your pet</div>
+                        <div class="font-light tracking-wider text-center">Pet International Day should be everyday</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo18.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">Read and Sip</div>
+                        <div class="font-light tracking-wider text-center">You choose the book, we choose the coffee</div>
+                    </div>
+                    <div class="">
+                        <div class="ring-2 ring-offset-4 ring-white">
+                            <img alt="logo" class="w-80 h-80" src="@/images/photo19.jpeg"/>
+                        </div>
+                        <div class=" pt-4 tracking-wider font-semibold text-base text-center">Future Events</div>
+                        <div class="font-light tracking-wider text-center">Stay tuned!</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-12 bg-tile px-12">
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-12 pb-8">
+                    <div class="flex flex-col space-y-2 text-white">
+                        <div class=" pt-4 tracking-wider font-semibold text-base">SHOPPING WITH US</div>
+                        <div class="font-light tracking-wider">Client’s Card</div>
+                        <div class="font-light tracking-wider">Gift Card</div>
+                        <div class="font-light tracking-wider">CupBook Plus</div>
+                        <div class="font-light tracking-wider">Special Orders</div>
+                    </div>
+                    <div class="flex flex-col space-y-2 text-white">
+                        <div class=" pt-4 tracking-wider font-semibold text-base">COSTUMER SERVICES</div>
+                        <div class="font-light tracking-wider">Help</div>
+                        <div class="font-light tracking-wider">Delivery</div>
+                        <div class="font-light tracking-wider">Payment Methods</div>
+                        <div class="font-light tracking-wider">Privacy Policy</div>
+                        <div class="font-light tracking-wider">Legal Notice</div>
+                    </div>
+                    <div class="flex flex-col space-y-2 text-white">
+                        <div class=" pt-4 tracking-wider font-semibold text-base">CONTACT US</div>
+                        <div class="font-light tracking-wider">CupBook</div>
+                        <div class="font-light tracking-wider">Adress: "Reshit Collaku" street, Tirana, Albania</div>
+                        <div class="font-light tracking-wider">+000 - 2428 -158</div>
+                        <div class="font-light tracking-wider">cupbook@gmail.com</div>
+                        <div class="font-light tracking-wider">www.cupbookshop.com</div>
+                    </div>
+                    <div class="flex flex-col space-y-2 text-white">
+                        <div class=" pt-4 tracking-wider font-semibold text-base">FOLLOW US</div>
+                        <div class="font-light tracking-wider">Facebook</div>
+                        <div class="font-light tracking-wider">Instagram</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bg-dark-tile text-cinnamonMilkDark text-center text-xs py-6">
+                <div>Copyright 2004 -2024 | All rights reserved.</div>
+                <div>This website or any portion thereof may not be reproduced or used in any manner whatsoever without the permission of the owner.</div>
+                <div>Terms & Conditions | Privacy | Legal Notice</div>
             </div>
         </div>
     </main>
